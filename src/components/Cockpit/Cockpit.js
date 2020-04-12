e0cb5098fa408b8ea7851fb25ee801117b15e0cb
Max Wang <max.wang@beteasy.com.au>
2020-04-12 12:36:28 +1000

    add hoc component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Cockpit() {
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect");
+    const timer = setTimeout(() => {
+      alert("Saved data to cloud!");
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+      console.log("[Cockpit.js] cleanup work in useEffect");
+    };
+  }, []);
   return <h1>Hi React App</h1>;
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,20 +2,16 @@ import React from "react";
 import "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import withClass from "../hoc/WithClass";
 
 function App() {
   const persons = [
-    { id: "1", name: "John", age: 35 },
+    { id: "1", name: "David", age: 9 },
     { id: "2", name: "Lucy", age: 6 },
     { id: "3", name: "Max", age: 28 },
   ];
 
-  return (
-    <div className="App">
-      <Cockpit />
-      <Persons persons={persons} />
-    </div>
-  );
+  return [<Cockpit key="c1" />, <Persons persons={persons} key="c2" />];
 }
 
-export default App;
+export default withClass(App, "App");
