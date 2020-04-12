85425c54e7dad6343f0f171c01b5e72e18c953fa
Max Wang <max.wang@beteasy.com.au>
2020-04-12 17:07:55 +1000

    add useRef in person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const Person = (props) => {
+  const inputElementRef = useRef();
+
+  useEffect(
+    () => inputElementRef.current && inputElementRef.current.focus(),
+    []
+  );
+
   return (
     <div>
       <h3>
         {props.name} {props.age} years old
       </h3>
-      <input type="text" onChange={props.change} value={props.name} />
+      <input
+        type="text"
+        ref={inputElementRef}
+        onChange={props.change}
+        value={props.name}
+      />
       <p>{props.children}</p>
     </div>
   );
 };
 
 Person.propTypes = {
+  change: PropTypes.func,
   name: PropTypes.string,
   age: PropTypes.number,
 };
